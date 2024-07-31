eb2b267960a11d5a1ece161769dfa67dcd0806e2
Tvintello <burkow2007@gmail.com>
2024-07-31 11:49:32 +0300

    added setup role implementation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord import Message
 from discord.ext import commands
-from config import TOKEN, PREFIX
+from config import TOKEN, PREFIX, COLOR_RED
 from censure import Censor
 from scripts.support import get_json
 from random import choice
@@ -12,6 +12,32 @@ bot = commands.Bot(PREFIX, intents=ins)
 
 censor_ru = Censor.get(lang="ru")
 phrases = get_json("scripts/phrases.json")
+
+
+bad_counter = {}
+
+
+class BotCog(commands.Cog):
+    role_name = "role name"
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def setup_role(self):
+        exists = False
+        for role in await self.bot.guilds[0].fetch_roles():
+            if role.name == self.role_name:
+                exists = True
+                break
+        if exists:
+            return
+
+        await self.bot.guilds[0].create_role(
+            name=self.role_name,
+            color=COLOR_RED,
+            hoist=True,
+            permissions=
+        )
 
 
 def get_profanity(text):
@@ -26,6 +52,11 @@ async def on_message(message: Message) -> None:
         await bot.process_commands(message)
 
         if get_profanity(message.content):
+            bad_counter[message.author] = bad_counter.setdefault(message.author, 0) + 1
+            role = discord.utils.get(message.author.roles, name="MUTED")
+            bot.get_context()
+            await message.author.add_roles(role)
+            print(bad_counter)
             await ctx.send(choice(phrases["on_swear"]))
             return
 
@@ -36,6 +67,12 @@ async def on_message(message: Message) -> None:
 @bot.command()
 async def say(ctx, message):
     await ctx.send(message)
+
+
+async def setup_bot(bot):
+    cog = BotCog(bot)
+    await bot.add_cog(cog)
+    await cog.setup_role()
 
 
 if __name__ == '__main__':
--- scripts/support.py
+++ scripts/support.py
@@ -2,6 +2,7 @@ import json
 
 
 def get_json(path):
-    with open(path) as f:
-        return json.loads(f.read())
+    with open(path, encoding="utf8") as f:
+        f = f.read()
+        return json.loads(f)
 
